7559cb283e47e12167a72dbe534e8c2f1e705873
LinaQiu <lqiu@ece.ubc.ca>
2017-10-14 18:40:12 -0400

    sensitive.py
    A test python script which includes internal bns url and potential password info.

---
+++ sensitive.py
@@ -0,0 +1,12 @@
+# This is a test python file, which includes sensitive information from ROCKYOU password dataset, and some URLs. 
+
+INTERNAL_URL = 'http://jira.agile.bns/'
+POTENTIAL_PASSWORD_LIST = ['123456', 'shadow', 'monkey']
+
+print("Internal URL is: "+INTERNAL_URL)
+
+i = 1
+for(item in POTENTIAL_PASSWORD_LIST):
+    print("Potential password "+i+": "+POTENTIAL_PASSWORD_LIST[i-1])
+    
+print("This is a test script.")
