feb4cf10197386ff64b5747cda9f306d4ad11384
LinaQiu <lqiu@ece.ubc.ca>
2017-10-14 18:47:18 -0400

    another_sensitive.py
    This is another python script including sensitive information, under branch "branch1".

---
+++ another_sensitive.py
@@ -0,0 +1,8 @@
+# This is another python script that includes sensitive information. 
+
+POTENTIAL_PASSWORD_LIST1 = ['jasmine', 'brandon', '666666', 'shadow', 'melissa', 'eminem']
+
+i = 0
+for item in POTENTIAL_PASSWORD_LIST1:
+    print("Pontential password "+i+": "+item)
+    i+=1
