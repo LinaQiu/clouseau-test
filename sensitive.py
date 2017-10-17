00e20af9deb8d0059ed6b245be303332fb20ba24
Lina Qiu <lina.qiu@scotiabank.com>
2017-10-17 16:38:44 -0400

    Added more url test strings.

--- sensitive.py
+++ sensitive.py
@@ -1,9 +1,14 @@
 # This is a test python file, which includes sensitive information from ROCKYOU password dataset, and some URLs. 
 
-INTERNAL_URL = 'http://jira.agile.bns/'
+INTERNAL_URL1 = 'http://jira.agile.bns/'
+INTERNAL_URL2 = 'http://jira.agile.com/'
+INTERNAL_URL3 = 'http://jira.agile.bns/' + 'https://jira.agile.bns/' + 'ftp://jira.agile.bns/'
+INTERNAL_URL3 = 'http://jira.agile.bns/' + 'www.google.com' + 'jira.agile.bns/'
 POTENTIAL_PASSWORD_LIST = ['123456', 'shadow', 'monkey']
 
-print("Internal URL is: "+INTERNAL_URL)
+print("Internal URL1 is: "+INTERNAL_URL1)
+print("Internal URL2 is: "+INTERNAL_URL2)
+print("Internal URL3 is: "+INTERNAL_URL3)
 
 i = 1
 for(item in POTENTIAL_PASSWORD_LIST):
